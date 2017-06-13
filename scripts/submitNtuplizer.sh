c29d9f51ad85d3c913a0e8961a2532b6873d8794
Matthias Mozer <mmozer@cern.ch>
2017-06-13 10:17:35 +0200

    adjust to new Tuilities feature

--- scripts/submitNtuplizer.sh
+++ scripts/submitNtuplizer.sh
@@ -5,7 +5,10 @@ if [ "$1" == "" ]; then
 else
     NAME="$1"
 fi
-submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_Data.txt --applyLumiMask "Collisions16" "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=0 crab=1
-submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC.txt "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
+
+SITE=T2_DE_DESY
+
+submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_Data.txt --applyLumiMask "Collisions16" "$NAME" --site=$SITE DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=0 crab=1 
+submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC.txt --site=$SITE "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1 
 #submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_MC_nonvalid.txt --allowNonValid "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
-submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_phys03.txt --inputDBS phys03 "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
+submit_job.py crabSubmit --sampleList DevTools/Ntuplizer/data/datasetList_phys03.txt--site=$SITE --inputDBS phys03 "$NAME" DevTools/Ntuplizer/test/MiniTree_cfg.py isMC=1 crab=1
